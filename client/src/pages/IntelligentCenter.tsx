6d08cd2268d593fe598ba01af87433b8751cec15
git-g-i-t <1579149298@qq.com>
2025-05-19 20:56:45 +0800

    完善定位功能

--- client/src/pages/IntelligentCenter.tsx
+++ client/src/pages/IntelligentCenter.tsx
@@ -242,24 +242,30 @@ const IntelligentCenter: React.FC = () => {
       async (position) => {
         try {
           // 通过后端代理获取城市信息
-          const response = await fetch(`/intelligent`, {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-              type: 'location',
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
-            })
-          });
+          // const response = await fetch(`/intelligent`, {
+          //   method: 'POST',
+          //   headers: {
+          //     'Content-Type': 'application/json',
+          //   },
+          //   body: JSON.stringify({
+          //     type: 'location',
+          //     lat: position.coords.latitude,
+          //     lng: position.coords.longitude
+          //   })
+          // });
           
+          // const data = await response.json();
+          // if (data.type === 'location') {
+          //   setSelectedCity(data.city);
+          //   // 自动触发天气查询
+          //   handleGetWeather(data.city);
+          // }
+          const apiKey = '1ef2b09e54904ea6bf07404436dec7a5 ';
+          const response = await fetch(`https://api.opencagedata.com/geocode/v1/json?q=${position.coords.latitude}+${position.coords.longitude}&key=${apiKey}`);
           const data = await response.json();
-          if (data.type === 'location') {
-            setSelectedCity(data.city);
-            // 自动触发天气查询
-            handleGetWeather(data.city);
-          }
+          const city = data.results[0].components.city;
+          setSelectedCity(city);
+          handleGetWeather(city);
         } catch (error) {
           message.error('获取位置失败');
         } finally {
